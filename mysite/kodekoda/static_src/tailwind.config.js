5559ebffd403f52a273ed19e54c18ae8f4619ddc
Haidar <68181532+hilmyha@users.noreply.github.com>
2022-06-02 01:25:15 +0700

    setting up tailwind config js

--- mysite/kodekoda/static_src/tailwind.config.js
+++ mysite/kodekoda/static_src/tailwind.config.js
@@ -43,6 +43,11 @@ module.exports = {
     ],
     theme: {
         extend: {},
+        screens: {
+            'tablet': '640px',
+            'laptop': '1024px',
+            'desktop': '1280px',
+        },
     },
     plugins: [
         /**
