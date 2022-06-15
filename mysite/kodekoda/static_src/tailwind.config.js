86e0340d5eb4142584f8956116348cbce3912a61
Haidar <68181532+hilmyha@users.noreply.github.com>
2022-06-15 13:01:59 +0700

    styling

--- mysite/kodekoda/static_src/tailwind.config.js
+++ mysite/kodekoda/static_src/tailwind.config.js
@@ -43,11 +43,6 @@ module.exports = {
     ],
     theme: {
         extend: {},
-        screens: {
-            'tablet': '640px',
-            'laptop': '1024px',
-            'desktop': '1280px',
-        },
     },
     plugins: [
         /**
